2264cac32d3408d739505b50acdeb56a1f62fad0
liuxiaoming <liuxiaoming@gevent.com.cn>
2020-07-27 23:22:46 +0800

    a

--- src/main/java/com/mind/product/eca/MTSConst.java
+++ src/main/java/com/mind/product/eca/MTSConst.java
@@ -1,6 +1,6 @@
 package com.mind.product.eca;
 
-public class MTSConst {
+public class mTSConst {
   public static final String DUBBO_SERVICE_URL="dubbo://localhost:20880";
   public static final int DUBBO_SERVICE_TIMEOUT=600000;
   
